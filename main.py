1b6b3b1833fa8c4568c839eb7cb5339997ab3ed1
Alejandro <lopalejandro@gmail.com>
2022-01-10 15:56:42 -0300

    Se agrego la busqueda y borrado de registros

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ class Pet(Model):
     class Meta:
         database = db # this model uses the "people.db" database
 
-
-
 def create_and_connect():
     db.connect()
     db.create_tables([Person,Pet])
@@ -30,6 +28,31 @@ def create_family_members():
     uncle_tomy.save()
 
     grandma_ana = Person.create(name="Ana",birthday=date(1967,12,20),is_relative=False)
+    grandma_Rosa = Person.create(name="Rosa", birthday=date(1967, 12, 20), is_relative=False)
+
+    tommy_dog=Pet.create(owner=uncle_tomy,name="Fido",animal_type="Perro")
+    ana_cat=Pet.create(owner=grandma_ana,name="Pelusa",animal_type="Gato")
+
+    tommy_dog.name="Firulais"
+    tommy_dog.save()
+
+def get_family_members():
+    for person in Person.select():
+        print("Nombre :{} Cumpleano {}".format(person.name, person.birthday))
+
+
+def get_family_member_birthday(name):
+    family_member =Person.select().where(Person.name== name).get()
+    print("{} cumple el : {}".format(name, family_member.birthday))
+
+def delete_pet(nombre):
+    query = Pet.delete().where(Pet.name== nombre)
+    deleted_entries = query.execute()
+    print("{} registros borrados".format(deleted_entries))
+
 
 create_and_connect()
-create_family_members()
+#create_family_members()
+#get_family_members()
+#get_family_member_birthday("Tommy")
+delete_pet("Pelusa")
